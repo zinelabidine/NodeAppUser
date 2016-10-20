ab9cb4ff15c9cc35ec2523e4dea2f0808e02c13a
Zine El Abidine HADJ CADI <zhadjcadi@gmail.com>
2016-10-20 16:46:52 +0200

    Create module to handle diffrents server response

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var employeeService = require('./lib/employees');
 
 http.createServer(function (req, res) {
 	// A parsed url to work with in case there are parameters
@@ -15,12 +16,23 @@ http.createServer(function (req, res) {
 	}
 	if (_url = /^\/employees$/i.exec(req.url)) {
 		// return a list of employees
-		res.writeHead(200);
-		return res.end('employee list');
+		employeeService.getEmployees(function (error, data) {
+		if (error) {
+			// send a 500 error
+		}
+			// send the data with a 200 status code
+		});
 	} else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
 		// find the employee by the id in the route
-		res.writeHead(200);
-		return res.end('a single employee');
+		employeeService.getEmployee(_url[1], function (error, data) {
+			if (error) {
+				// send a 500 error
+			}
+			if (!data) {
+				// send a 404 error
+			}
+			// send the data with a 200 status code
+		});
 	} else {
 		// try to send the static file
 		res.writeHead(200);
