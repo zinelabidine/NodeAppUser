def9750502a65d737316f6b6260d567c52f02ee8
Zine El Abidine HADJ CADI <zhadjcadi@gmail.com>
2016-10-20 16:56:58 +0200

    Use lib/responseGenerator in main server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var employeeService = require('./lib/employees');
+var responseService = require('./lib/responseGenerator');
 
 http.createServer(function (req, res) {
 	// A parsed url to work with in case there are parameters
@@ -17,21 +18,26 @@ http.createServer(function (req, res) {
 	if (_url = /^\/employees$/i.exec(req.url)) {
 		// return a list of employees
 		employeeService.getEmployees(function (error, data) {
-		if (error) {
-			// send a 500 error
-		}
+			if (error) {
+				// send a 500 error
+				return responseService.send500(error, res);
+			}
 			// send the data with a 200 status code
+			responseService.sendJson(data, res);
 		});
 	} else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
 		// find the employee by the id in the route
 		employeeService.getEmployee(_url[1], function (error, data) {
 			if (error) {
 				// send a 500 error
+				return responseService.send500(error, res);
 			}
 			if (!data) {
 				// send a 404 error
+				return responseService.send404(res);
 			}
 			// send the data with a 200 status code
+			responseService.sendJson(data, res);
 		});
 	} else {
 		// try to send the static file
